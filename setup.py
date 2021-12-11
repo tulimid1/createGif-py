60819b44ae538db9b8d3220f470faaa094a2feca
Duncan Tulimieri <tulimid@udel.edu>
2021-12-11 15:32:08 -0500

    update without os as require

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
   download_url = 'https://github.com/tulimid1/savingfig2/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['saving', 'figures', 'gifs'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'os',
           'datetime',
           'matplotlib',
           'imageio', 
