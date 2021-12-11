1a7c6d7168d8f9a78e05c314240ea6142b47018b
Duncan Tulimieri <tulimid@udel.edu>
2021-12-11 15:37:57 -0500

    update again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'savingfig2',         # How you named your package folder (MyLib)
   packages = ['savingfig2'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Save figures and create gifs in an organized manner',   # Give a short description about your library
   author = 'Duncan Tulimieri',                   # Type in your name
