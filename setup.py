94050b98822d72701de24a9ba505416f55e5a1ff
Duncan Tulimieri <tulimid@udel.edu>
2022-02-15 17:34:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
   download_url = 'https://github.com/tulimid1/savingfigR/archive/refs/tags/1.0.3.tar.gz',    # I explain this later on
   keywords = ['figures', 'gifs', 'save'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'datetime', 
           'matplotlib', 
           'imageio',
       ],
